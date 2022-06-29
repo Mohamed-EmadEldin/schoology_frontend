8a4db2eddc18e38f17eade1a1b53cce906a03421
Private <msaleh.abdelrahman@gmail.com>
2022-06-29 07:16:07 +0200

    routing and 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { CoreModule } from "./core/core.module";
 import {RouterModule} from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 @NgModule({
   declarations: [
