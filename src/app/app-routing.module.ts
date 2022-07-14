ee487795b83de979a60ab5e04681659c9f5574df
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-07-14 22:01:46 +0200

    activate parent guard  is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import {AuthGuard} from "./guards/auth.guard";
 import {UnauthorizedComponent} from "./core/unauthorized/unauthorized.component";
 import {IsTeacherGuard} from "./guards/is-teacher.guard";
 import {IsStudentGuard} from "./guards/is-student.guard";
+import {IsParentGuard} from "./guards/is-parent.guard";
 
 
 const routes:Routes = [
@@ -42,7 +43,8 @@ const routes:Routes = [
     loadChildren: () => import('./student/student.module').then(m => m.StudentModule),
     canActivate: [AuthGuard, IsStudentGuard]
   },
-  {path: 'parent', loadChildren: () => import('./parent/parent.module').then(m => m.ParentModule)},
+  {path: 'parent', loadChildren: () => import('./parent/parent.module').then(m => m.ParentModule),
+    canActivate: [AuthGuard, IsParentGuard]},
   {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
   {path: "un-auth", component: UnauthorizedComponent},
   {path: '**', component: NotFoundComponent},
