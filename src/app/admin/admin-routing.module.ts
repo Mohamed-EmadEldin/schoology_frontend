9143b65ee00f6060f94d0818d30148c3a756c39a
Private <msaleh.abdelrahman@gmail.com>
2022-07-16 04:16:28 +0200

    admin main page

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './admin.component';
+import {AdminMainComponent} from "./admin-main/admin-main.component";
+import {MeetingCrudComponent} from "./meeting-crud/meeting-crud.component";
+import {ExamsCrudComponent} from "./exams-crud/exams-crud.component";
+import {UsersCrudComponent} from "./users-crud/users-crud.component";
+import {ClassCrudComponent} from "./class-crud/class-crud.component";
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  { path: '', component: AdminMainComponent },
+  { path: 'meet-crud', component: MeetingCrudComponent},
+  { path: 'exam-crud', component: ExamsCrudComponent},
+  { path: 'user-crud', component: UsersCrudComponent},
+  { path: 'class-crud', component: ClassCrudComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,16 +2,46 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { AdminComponent } from './admin.component';
+import { AdminMainComponent } from './admin-main/admin-main.component';
+import {CardModule} from "primeng/card";
+import { MeetingCrudComponent } from './meeting-crud/meeting-crud.component';
+import { ExamsCrudComponent } from './exams-crud/exams-crud.component';
+import { UsersCrudComponent } from './users-crud/users-crud.component';
+import { ClassCrudComponent } from './class-crud/class-crud.component';
+import {TableModule} from "primeng/table";
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule} from "@angular/forms";
+import {DropdownModule} from "primeng/dropdown";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
+import {ToastModule} from "primeng/toast";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
+import {ToolbarModule} from "primeng/toolbar";
+import {DialogModule} from "primeng/dialog";
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminMainComponent,
+    MeetingCrudComponent,
+    ExamsCrudComponent,
+    UsersCrudComponent,
+    ClassCrudComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    CardModule,
+    TableModule,
+    InputTextModule,
+    FormsModule,
+    DropdownModule,
+    ButtonModule,
+    RippleModule,
+    ToastModule,
+    ConfirmDialogModule,
+    ToolbarModule,
+    DialogModule
   ]
 })
 export class AdminModule { }
