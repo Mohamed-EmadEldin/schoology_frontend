bdab00066d75cba3d1ef7a76ba36876a358c414c
Mohamed-EmadEldin <mohamed.emad1719@gmail.com>
2022-07-16 18:17:45 +0200

    make slider in dashboard

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -13,4 +13,19 @@ export class DashboardComponent implements OnInit {
   }
 
   selected!: Date | null;
+
+  imageObject: Array<object> = [{
+    image: 'assets/img/slider/1.jpg',
+    thumbImage: 'assets/img/slider/1_min.jpeg',
+    alt: 'alt of image',
+    title: 'title of image'
+}, {
+    image: '.../iOe/xHHf4nf8AE75h3j1x64ZmZ//Z==', // Support base64 image
+    thumbImage: '.../iOe/xHHf4nf8AE75h3j1x64ZmZ//Z==', // Support base64 image
+    title: 'Image title', //Optional: You can use this key if want to show image with title
+    alt: 'Image alt', //Optional: You can use this key if want to show image with alt
+    order: 1 //Optional: if you pass this key then slider images will be arrange according @input: slideOrderType
+}
+];
+
 }
