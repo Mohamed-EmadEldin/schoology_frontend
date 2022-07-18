7d4b2ebeb9bf0219ffdc3deb4bf6718ac19cf493
Private <msaleh.abdelrahman@gmail.com>
2022-07-19 01:49:29 +0200

    admin create meeting 'has issue'

--- src/app/admin/meeting-crud/meeting-crud.component.ts
+++ src/app/admin/meeting-crud/meeting-crud.component.ts
@@ -3,6 +3,9 @@ import {Meeting} from "../../models/meeting";
 import {ConfirmationService, MessageService} from "primeng/api";
 import {MeetingCrudService} from "../../services/admin/meeting-crud.service";
 import {Table} from "primeng/table";
+import {ICourse, ITeacher, IUiClass} from "../../Interfaces";
+import {HelperService} from "../../services/admin/helper.service";
+import {formatDate} from "@angular/common";
 
 @Component({
   selector: 'app-meeting-crud',
@@ -14,19 +17,85 @@ export class MeetingCrudComponent implements OnInit {
 
   _meetings: Meeting[] = []
 
+  _meeting: Meeting = new Meeting()
+
+  teachers: ITeacher[] = []
+  selectedTeacherId: number = -1
+
+  teacherClasses: IUiClass[] = []
+  selectedClass: number = -1
+
+  teacherCourses: ICourse[] = []
+  selectedCourse: number = -1
+
+  disableClassSelect: boolean = true;
+  disableCourseSelect: boolean = true;
+  isTeacherHasClasses: boolean = false;
+  isTeacherSelected: boolean = false;
+
+  invalid: boolean = false; //check create form status
+
   clonedItems: { [s: string]: any; } = {}; //to help restore row if update is cancelled
 
   displayCreateModel: boolean = false; //boolean to show create dialog on request
 
   selectedMeetings: Meeting[] = []
 
-  constructor(private messageService: MessageService,
-              private confirmationService: ConfirmationService,
-              private meetingCrudService: MeetingCrudService) { }
+  constructor(private meetingCrudService: MeetingCrudService,
+              private helperService: HelperService,
+              private messageService: MessageService,
+              private confirmationService: ConfirmationService) { }
 
   ngOnInit(): void {
     this.meetingCrudService.getMeetings()
       .subscribe(data => this._meetings = data)
+  }
+
+  openCreateDialog() {
+    this.helperService.getAllTeachers().subscribe(data => {
+      this.teachers = data.map(value => {
+        return {name: value.name, id: value.id}
+      });
+    })
+    this.displayCreateModel = true;
+  }
+
+  getClasses(teacherId: number) {
+    this.helperService.getTeacherClasses(teacherId).subscribe(data => {
+      this.teacherClasses = data.map(value => {
+        return {name: value.name, code: value.id}
+      });
+    });
+  }
+
+  getCourses(teacherId: number) {
+    this.helperService.getTeacherCourse(teacherId).subscribe(data => {
+      this.teacherCourses = data.map(value => {
+        return {name: value.name, id: value.id}
+      });
+    });
+  }
+
+  async activateCourseAndClassDropdown(teacherId: number) {
+    await this.getClasses(teacherId)
+    await this.getCourses(teacherId)
+    this.isTeacherHasClasses = true;
+    this.disableClassSelect = false;
+    this.disableCourseSelect = false;
+    this.isTeacherSelected = true;
+  }
+
+  createMeeting() { //TODO: check admin create meeting
+    this.invalid = false
+    this._meeting.date = formatDate(this._meeting.date, 'YYYY-MM-dd', 'en')
+    this.meetingCrudService.createMeeting({
+      ...this._meeting,
+      teacherId: this.selectedTeacherId,
+      classId: this.selectedClass,
+      courseId: this.selectedCourse,
+    })
+    this.displayCreateModel = false;
+    this.messageService.add({severity: 'success', summary: 'Success', detail: 'Exam was created successfully'});
   }
 
   onRowEditInit(_meeting: any) {
@@ -54,8 +123,10 @@ export class MeetingCrudComponent implements OnInit {
       accept: () => {
         this._meetings = this._meetings.filter(val => val.classId !== _meeting.id);
         this.meetingCrudService.deleteMeet(_meeting.id)
-          .subscribe(() => this._meetings.splice(_meeting.id,1))
-        this.messageService.add({severity:'success', summary: 'Successful', detail: 'Meeting Deleted', life: 3000});
+          .subscribe(() => {
+            this._meetings.splice(_meeting.id, 1);
+            this.messageService.add({severity:'success', summary: 'Successful', detail: 'Meeting Deleted', life: 3000});
+          })
       }
     });
   }
