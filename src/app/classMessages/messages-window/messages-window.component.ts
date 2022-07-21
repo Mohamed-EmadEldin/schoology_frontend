0277afb2189a216bc837a7663b973f9d5a797b55
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-07-21 11:59:18 +0200

    add time to the new sent message

--- src/app/classMessages/messages-window/messages-window.component.ts
+++ src/app/classMessages/messages-window/messages-window.component.ts
@@ -1,11 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterContentInit, Component, Input, OnInit} from '@angular/core';
 import {StateService} from "../../services/state.service";
 import {MessagesService} from "../../services/messages.service";
 import {Message} from "../../models/Message";
 import {MessageService} from "primeng/api";
 
-class UiMessage{
-  constructor(public senderId=-1,public receiverId=-1,public message='',public sender:any) {
+class UiMessage {
+  constructor(public senderId = -1, public receiverId = -1, public message = '', public sender: any) {
 
   }
 
@@ -17,15 +17,23 @@ class UiMessage{
   styleUrls: ['./messages-window.component.css']
 })
 
-export class MessagesWindowComponent implements OnInit {
+export class MessagesWindowComponent implements OnInit, AfterContentInit {
 
   @Input() otherId: number = -1
-  @Input() senderName:string = ""
+  @Input() senderName: string = ""
   public myId: number = -1
   public messages: any = []
-  public newMessage:any =""
+  public newMessage: any = ""
 
-  constructor(public stateService: StateService, public appMessageService: MessagesService,private messageService: MessageService) {
+  constructor(public stateService: StateService, public appMessageService: MessagesService, private messageService: MessageService) {
+  }
+
+  ngAfterContentInit(): void {
+    let message_container = document.getElementById('messages_container')
+    if (message_container) {
+      console.log(message_container)
+      message_container.scrollTop = message_container.scrollHeight
+    }
   }
 
   ngOnInit(): void {
@@ -33,22 +41,35 @@ export class MessagesWindowComponent implements OnInit {
     console.log(this.otherId)
     this.appMessageService.getMessages(this.otherId).subscribe({
       next: (messages) => {
-        this.messages =messages
+        this.messages = messages
         console.log(messages)
+        let message_container = document.getElementById('messages_container')
+        if (message_container) {
+          console.log(message_container)
+          message_container.scrollTop = message_container.scrollHeight
+        }
+
       }
     })
   }
 
   sendMessage() {
-    let message = new Message(this.newMessage,this.otherId)
+    let message = new Message(this.newMessage, this.otherId)
+    this.newMessage = ''
     console.log(message)
-    this.appMessageService.sendMessages(message).subscribe({next:(message)=>{
+    this.appMessageService.sendMessages(message).subscribe({
+      next: (message) => {
         this.messages.push(message)
-        this.messageService.add({severity:'success', summary:'Success', detail:`your message has been sent`});
-
+        console.log(message)
+        this.messageService.add({severity: 'success', summary: 'Success', detail: `your message has been sent`});
+        // let message_container = document.getElementById('messages_container')
+        // if (message_container) {
+        //   console.log(message_container)
+        //   message_container.scrollTop = message_container.scrollHeight
+        // }
       },
-      error:()=>{
-        this.messageService.add({severity:'error', summary:'Error', detail:'Your message has not been sent !!'})
+      error: () => {
+        this.messageService.add({severity: 'error', summary: 'Error', detail: 'Your message has not been sent !!'})
       }
     })
     // this.messages.push(new UiMessage(this.myId,this.otherId,this.newMessage,{name:this.stateService.getState().userName}))
